b5641bf8e15f76a008ce05e7f9645a6370f28636
vgregory <vinicius.gregory@gmail.com>
2017-01-30 11:42:11 -0200

    defining class Article

--- articles.py
+++ articles.py
@@ -1,2 +1,7 @@
 # This class should be a representation of an article (something the user would
 # like to read and take notes about it).
+
+
+class Article:
+    """Not yet defined how to handle articles"""
+    pass
